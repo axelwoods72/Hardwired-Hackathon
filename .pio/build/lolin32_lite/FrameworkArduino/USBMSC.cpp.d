deba0955c26238de9230f14b23dceb63ad9e7429
Axel Woods <axelwoods72@gmail.com>
2026-07-24 16:36:43 +1000

    ignore .pio

--- .pio/build/lolin32_lite/FrameworkArduino/USBMSC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/lolin32_lite/FrameworkArduino/USBMSC.cpp.o: \
- C:/Users/blake/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
- C:/Users/blake/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/blake/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
